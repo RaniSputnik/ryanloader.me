27b571a343f6612ea2995d75d16efa7d9d299adf
Ryan Loader <talk@ryanloader.me>
2015-04-06 21:49:07 +1200

    Added "latest blog posts" to main page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+
+// Set all the targets of the links to open in a new tab
 $("#links-wrapper a").each(function(index){
 	this.target="_blank";
 });
+
+// Load the latest blog post
+var $blogData = $("#blog-data");
+$blogData.rss("http://0nf0rm.blogspot.com/feeds/posts/default?alt=rss", {
+    limit: 5,
+    layoutTemplate: '<dl class="dl-horizontal">{entries}</dl>',
+    entryTemplate: '<dt><a href="{url}">{title} - {date}</a></dt><dd>{shortBody}</dd>',
+    onData: function(){
+    	$('#blog-loading').remove();
+    },
+    error: function(){
+    	$blogData.empty().html('Sorry, no blog today. You could try <a href="http://blog.ryanloader.me" target="_blank">taking a more direct route.</a>');
+    },
+
+}).show();
+
